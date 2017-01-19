ef87976e32545f7c7f115ef5581517ea5211f8e6
Les Hazlewood <les@hazlewood.com>
2017-01-18 16:30:08 -0800

    added spring security dependencies for ss testing

--- src/main/java/com/stormpath/examples/origin/SecurityConfig.java
+++ src/main/java/com/stormpath/examples/origin/SecurityConfig.java
@@ -9,6 +9,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll();
     }
 }
